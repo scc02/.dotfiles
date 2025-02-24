bed75eb3322e74883851af5a73c1279b2deb3d74
Scc <sccong@outlook.com>
2025-02-24 10:06:44 +0800

    update

--- fish/config.fish
+++ fish/config.fish
@@ -98,3 +98,5 @@ set -x PATH $PATH $ANDROID_HOME/emulator $ANDROID_HOME/platform-tools
 
 set -Ux VISUAL nvim      # 选择你喜欢的编辑器，如 vim、nano 等
 set -Ux EDITOR $VISUAL   # 使 $EDITOR 指向 $VISUAL
+
+export GITHUB_TOKEN="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"
