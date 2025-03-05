e65ac3b353ea4260a48628db72344b208b98dc28
Scc <sccong@outlook.com>
2025-03-05 10:56:49 +0800

    update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -427,7 +427,7 @@ require("lazy").setup({
       completion = {
         list = {
           selection = {
-            preselect = true
+            -- preselect = true
           }
         },
         accept = {
