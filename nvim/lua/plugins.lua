556c4cabd3e3590f9f51fd4c0b96419cdb93a408
Scc <sccong@outlook.com>
2025-03-10 10:35:43 +0800

    update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -424,6 +424,13 @@ require("lazy").setup({
       --   use_nvim_cmp_as_default = true,
       --   nerd_font_variant = 'mono'
       -- },
+      fuzzy = {
+        sorts = {
+          'exact',
+          'score',
+          'sort_text',
+        },
+      },
       completion = {
         list = {
           selection = {
