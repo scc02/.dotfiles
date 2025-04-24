63fddecacec080a9e468f56a77b69774a8c5e0bc
Scc <sccong@outlook.com>
2025-04-24 11:21:20 +0800

    update

--- nvim/lua/conf/diffview.lua
+++ nvim/lua/conf/diffview.lua
@@ -3,13 +3,20 @@ local actions = require("diffview.actions")
 require("diffview").setup({
   show_help_hints = false,
   keymaps = {
-    disable_defaults = false,                -- Disable the default keymaps
+    disable_defaults = false,          -- Disable the default keymaps
     view = {
-      ["gf"] = actions.goto_file_edit,       -- Open the file in a new split in the previous tabpage
+      ["gf"] = actions.goto_file_edit, -- Open the file in a new split in the previous tabpage
     },
     file_panel = {
     },
     file_history_panel = {
+      ["gf"] = function()
+        actions.goto_file_edit()
+        vim.defer_fn(function()
+          vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+        end, 0)
+      end,
+
     },
     option_panel = {
       ["<tab>"] = actions.select_entry,
@@ -42,4 +49,3 @@ vim.opt.diffopt = {
   'linematch:200',
   'indent-heuristic',
 }
-
