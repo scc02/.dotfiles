fe843c89ce3de0127773a1d5057de28e1db0cacc
chencong <sccong@outlook.com>
2025-04-28 19:39:09 +0800

    update

---
+++ nvim/lua/conf/blink.lua
@@ -0,0 +1,137 @@
+local blink = require('blink-cmp')
+blink.setup({
+  cmdline = {
+    completion = {
+      menu = {
+        auto_show = function(ctx)
+          return vim.fn.getcmdtype() == ':'
+          -- enable for inputs as well, with:
+          -- or vim.fn.getcmdtype() == '@'
+        end,
+      },
+      ghost_text = { enabled = false }
+    },
+    keymap = {
+      ["<CR>"] = { "select_and_accept", "fallback" }
+    }
+  },
+  snippets = { preset = 'luasnip' },
+  -- appearance = {
+  --   use_nvim_cmp_as_default = true,
+  --   nerd_font_variant = 'mono'
+  -- },
+  fuzzy = {
+    sorts = {
+      'exact',
+      'score',
+      'sort_text',
+    },
+  },
+  completion = {
+    list = {
+      selection = {
+        -- preselect = true
+      }
+    },
+    accept = {
+      auto_brackets = {
+        enabled = false,
+      },
+    },
+    menu = {
+      border = 'rounded',
+      draw = {
+        -- padding = 1,
+        -- gap = 4,
+        columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind", gap = 1 } },
+        components = {
+          kind = {
+            text = function(ctx)
+              local len = 10 - string.len(ctx.kind)
+              local space = string.rep(" ", len)
+              return ctx.kind .. space .. '[' .. ctx.source_name .. ']'
+            end
+          }
+        },
+      }
+    },
+    documentation = {
+      window = { border = 'rounded' },
+      auto_show = true,
+      auto_show_delay_ms = 0,
+    },
+  },
+
+  keymap = {
+    ["<C-e>"] = { "hide", "fallback" },
+    ["<CR>"] = {
+      function(cmp)
+        local mode = vim.api.nvim_get_mode().mode
+        if mode == 'c' then
+          return nil
+        end
+
+        local selected_item = require('blink.cmp.completion.list').get_selected_item()
+        if cmp.is_visible then
+          if selected_item ~= nil then
+            return cmp.accept()
+          else
+            return cmp.accept({ index = 1 })
+          end
+        end
+      end,
+      "fallback"
+    },
+
+    ["<Tab>"] = {
+      function(cmp)
+        return cmp.select_next()
+      end,
+      "snippet_forward",
+      "fallback",
+    },
+    ["<S-Tab>"] = {
+      function(cmp)
+        return cmp.select_prev()
+      end,
+      "snippet_backward",
+      "fallback",
+    },
+  },
+  sources = {
+    default = { 'snippets', 'lsp', 'path', 'buffer' },
+    providers = {
+      codeium = {
+        name = "codeium",
+        module = "blink.compat.source",
+      },
+      snippets = {
+        min_keyword_length = 1,
+        score_offset = 4,
+      },
+      cmdline = {
+        min_keyword_length = function(ctx)
+          -- when typing a command, only show when the keyword is 3 characters or longer
+          if ctx.mode == 'cmdline' and string.find(ctx.line, ' ') == nil then return 3 end
+          return 0
+        end
+      },
+      lsp = {
+        min_keyword_length = 0,
+        score_offset = 3,
+        name = "LSP",
+        module = "blink.cmp.sources.lsp",
+        fallbacks = {},
+      },
+      path = {
+        min_keyword_length = 0,
+        score_offset = 2,
+      },
+      buffer = {
+        min_keyword_length = 1,
+        score_offset = 1,
+      },
+    },
+  },
+
+})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -437,7 +437,6 @@ require("lazy").setup({
   {
     "saghen/blink.cmp",
     dependencies = {
-      "saghen/blink.compat",
       {
         'L3MON4D3/LuaSnip',
         version = 'v2.*',
@@ -453,145 +452,10 @@ require("lazy").setup({
       }
     },
     version = "*",
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      cmdline = {
-        completion = {
-          menu = {
-            auto_show = function(ctx)
-              return vim.fn.getcmdtype() == ':'
-              -- enable for inputs as well, with:
-              -- or vim.fn.getcmdtype() == '@'
-            end,
-          },
-          ghost_text = { enabled = false }
-        },
-        keymap = {
-          ["<CR>"] = { "select_and_accept", "fallback" }
-        }
-      },
-      snippets = { preset = 'luasnip' },
-      -- appearance = {
-      --   use_nvim_cmp_as_default = true,
-      --   nerd_font_variant = 'mono'
-      -- },
-      fuzzy = {
-        sorts = {
-          'exact',
-          'score',
-          'sort_text',
-        },
-      },
-      completion = {
-        list = {
-          selection = {
-            -- preselect = true
-          }
-        },
-        accept = {
-          auto_brackets = {
-            enabled = false,
-          },
-        },
-        menu = {
-          border = 'rounded',
-          draw = {
-            -- padding = 1,
-            -- gap = 4,
-            columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind", gap = 1 } },
-            components = {
-              kind = {
-                text = function(ctx)
-                  local len = 10 - string.len(ctx.kind)
-                  local space = string.rep(" ", len)
-                  return ctx.kind .. space .. '[' .. ctx.source_name .. ']'
-                end
-              }
-            },
-          }
-        },
-        documentation = {
-          window = { border = 'rounded' },
-          auto_show = true,
-          auto_show_delay_ms = 0,
-        },
-      },
-
-      keymap = {
-        ["<C-e>"] = { "hide", "fallback" },
-        ["<CR>"] = {
-          function(cmp)
-            local mode = vim.api.nvim_get_mode().mode
-            if mode == 'c' then
-              return nil
-            end
-
-            local selected_item = require('blink.cmp.completion.list').get_selected_item()
-            if cmp.is_visible then
-              if selected_item ~= nil then
-                return cmp.accept()
-              else
-                return cmp.accept({ index = 1 })
-              end
-            end
-          end,
-          "fallback"
-        },
-
-        ["<Tab>"] = {
-          function(cmp)
-            return cmp.select_next()
-          end,
-          "snippet_forward",
-          "fallback",
-        },
-        ["<S-Tab>"] = {
-          function(cmp)
-            return cmp.select_prev()
-          end,
-          "snippet_backward",
-          "fallback",
-        },
-      },
-      sources = {
-        default = { 'snippets', 'lsp', 'path', 'buffer' },
-        providers = {
-          codeium = {
-            name = "codeium",
-            module = "blink.compat.source",
-          },
-          snippets = {
-            min_keyword_length = 1,
-            score_offset = 4,
-          },
-          cmdline = {
-            min_keyword_length = function(ctx)
-              -- when typing a command, only show when the keyword is 3 characters or longer
-              if ctx.mode == 'cmdline' and string.find(ctx.line, ' ') == nil then return 3 end
-              return 0
-            end
-          },
-          lsp = {
-            min_keyword_length = 0,
-            score_offset = 3,
-            name = "LSP",
-            module = "blink.cmp.sources.lsp",
-            fallbacks = {},
-          },
-          path = {
-            min_keyword_length = 0,
-            score_offset = 2,
-          },
-          buffer = {
-            min_keyword_length = 1,
-            score_offset = 1,
-          },
-        },
-      },
-    },
-    opts_extend = { "sources.default" }
+    opts_extend = { "sources.default" },
+    config = function ()
+      require("conf.blink")
+    end
   },
   {
     "ibhagwan/fzf-lua",
@@ -634,7 +498,11 @@ require("lazy").setup({
   {
     'sontungexpt/better-diagnostic-virtual-text',
     config = function(_)
-      require('better-diagnostic-virtual-text').setup({})
+      require('better-diagnostic-virtual-text').setup({
+        ui = {
+          wrap_line_after = true
+        }
+      })
     end
   },
 }, {
