7871754c350a37ed560927bf168e99603539207a
Scc <sccong@outlook.com>
2025-07-16 15:19:12 +0800

    test

--- nvim/lua/util/is_git.lua
+++ nvim/lua/util/is_git.lua
@@ -1,10 +1,3 @@
 local M = {}
-M.is_git_dir = function()
-  local cmd = io.popen("git rev-parse --is-inside-work-tree 2>/dev/null")
-  local result = cmd:read("*a")
-  cmd:close()
-
-  return result == "true\n"
-end
 
 return M
