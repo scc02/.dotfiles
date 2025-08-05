0a2f7599f77dd9b1cfd9d6b5a1c034b7543cb9ae
Scc <sccong@outlook.com>
2025-08-05 15:10:34 +0800

    update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -452,7 +452,7 @@ require("lazy").setup({
     },
     version = "*",
     opts_extend = { "sources.default" },
-    config = function ()
+    config = function()
       require("conf.blink")
     end
   },
@@ -493,6 +493,17 @@ require("lazy").setup({
       -- vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
     end
   },
+  {
+    "WilliamHsieh/overlook.nvim",
+    opts = {},
+
+    -- Optional: set up common keybindings
+    keys = {
+      { "<leader>pd", function() require("overlook.api").peek_definition() end, desc = "Overlook: Peek definition" },
+      { "<leader>pc", function() require("overlook.api").close_all() end,       desc = "Overlook: Close all popup" },
+      { "<leader>pu", function() require("overlook.api").restore_popup() end,   desc = "Overlook: Restore popup" },
+    },
+  }
 
   -- {
   --   'sontungexpt/better-diagnostic-virtual-text',
