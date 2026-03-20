304af426104c9ab190815b8da74408f7e96c0fd8
Scc <sccong@outlook.com>
2026-03-20 10:04:28 +0800

    update

--- nvim/lua/vscode-editor/keymaps.lua
+++ nvim/lua/vscode-editor/keymaps.lua
@@ -108,21 +108,16 @@ map('n', '<leader>.', function()
   require('vscode').action('editor.action.quickFix')
 end)
 
--- if vim.env.VSCODE_CLIENT_TYPE == 'cursor' then
-  -- vim.api.nvim_create_autocmd("InsertLeave", {
-  --   callback = function()
-  --     require('vscode').action('editor.cpp.disableenabled')
-  --   end
-  -- });
-  -- vim.api.nvim_create_autocmd("InsertEnter", {
-  --   callback = function()
-  --     require('vscode').action('editor.action.enableCppGlobally')
-  --   end
-  -- });
--- else
---   -- VSCode 逻辑
--- end
-
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function()
+    require('vscode').action('editor.cpp.disableenabled')
+  end
+});
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = function()
+    require('vscode').action('editor.action.enableCppGlobally')
+  end
+});
 
 map('n', 'za', function()
   require('vscode').action('editor.toggleFold')
